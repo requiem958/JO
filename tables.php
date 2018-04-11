8555ecea5512e35c4601a924e3ff9b87a844d214
Marius Monnier <marius.monnier@etu.univ-grenoble.fr>
2018-04-11 17:02:59 +0200

    sfjkdhsfdé

--- tables.php
+++ tables.php
@@ -1,33 +1,55 @@
 <?php
 	$titre = 'Relations appartenant au compte ' . $login;
 	include('entete.php');
+	
+	
+	// définition des relations
+$lesRelations = array ("LesBatiments", "LesDisciplines", "LesEpreuves", "LesEquipes", "LesInscriptions", "LesLocataires", "LesLogements", "LesResultats", "LesSportifs", "LesBillets", "LesDossiers_base", "LesDossiers" );
 
-	// construction de la requete
-	$requete = "select table_name from tabs ";
+// définition des attributs
+$lesSchemas = array ("LesBatiments"  => array("nomBat","numero", "rue", "ville"),
+             "LesDisciplines" => array ("discipline"),
+		     "LesEpreuves" => array("nEpreuve", "nomE", "forme", "discipline", "categorie", "nbs", "dateEpreuve"),
+		     "LesEquipes" => array("nEquipe", "nSportif"),
+		     "LesInscriptions" => array("nInscrit", "nEpreuve"),
+		     "LesLocataires" => array("nSportif", "nLogement", "nomBat"),
+		     "LesLogements" => array("nLogement", "capacite", "nomBat"),
+		     "LesResultats" => array("nEpreuve", "gold", "silver", "bronze"),
+             "LesSportifs" => array("nSportif", "nomS", "prenomS", "pays", "categorie", "dateNais"),
+             "LesBillets"  => array("nDossier", "nBillet","nEpreuve"),
+             "LesDossiers_base" => array("nDossier", "nUtil", "dateEmission"),
+             "LesDossiers" => array("nDossier", "nUtil", "dateEmission", "prix")
+	);
 
-	// analyse de la requete et association au curseur
-	$curseur = oci_parse ($lien, $requete) ;
-
-	// execution de la requete
-	oci_execute ($curseur) ;
-
-	$res = oci_fetch ($curseur);
-
-	if ( !$res) {
-		// le resultat est vide
-		echo "<p class=\"erreur\"><b>Aucune relation </b></p>" ;
+	// pour chaque relation
+	foreach ($lesRelations as $uneRelation) {
+		// construction de la requête
+		$requete = "select * from $uneRelation ORDER BY {$lesSchemas[$uneRelation][0]}";
+		// analyse de la requete et association au curseur
+		$curseur = oci_parse ($lien, $requete) ;
+		// execution de la requete
+		oci_execute ($curseur);
+		if (!($row = oci_fetch_assoc ($curseur))) {
+			// le resultat est vide
+			echo "<p><b>La relation ".$uneRelation." est vide </b></p>" ;
+		}
+		else {
+			// création de la table qui va servir a la mise en page du resultat
+			echo "<p><table> <tr><th> ".$uneRelation." </th></tr><tr>" ;
+			foreach ($lesSchemas[$uneRelation] as $unAttr)
+				echo "<td><th> ".$unAttr." </th></td>";
+			echo "</tr>";
+			// Affichage du resultat (non vide)
+			do {
+				echo "<tr>";
+				foreach ($lesSchemas[$uneRelation] as $unAttr) {
+				  echo "<td> ".$row[strtoupper($unAttr)]." </td>";
+				}
+				echo "</tr>";
+			} while ($row = oci_fetch_assoc ($curseur));
+			echo "</table></p>";
+		}
+		oci_free_statement($curseur);
 	}
-	else {
-		// la table qui va servir a la mise en page du resultat
-		echo "<table> <tr><th> Nom relation </th></tr>" ;
-
-		// Affichage du resultat (non vide)
-		do {    $nomRel = oci_result($curseur,1) ;
-			echo "<tr><td> $nomRel </td></tr>";
-		} while (oci_fetch ($curseur));
-		echo "</table>";
-	}
-	oci_free_statement($curseur);
-
 	include('pied.php');
 ?>
