082b49bbfd227a64e3a62b66543975c631c2f739
requiem958 <requiemandsahariel@gmail.com>
2018-05-03 13:37:31 +0200

    e

--- GererVentes.php
+++ GererVentes.php
@@ -6,7 +6,7 @@
 	<form method=\"post\" action=\"GererVentes-acte1.php\">
 		<label for=\"nvUtil\">Êtes vous un nouvel utilisateur ?</label>
 		<input type=\"checkbox\" id=\"yes\" name=\"nvUtil\"></input>
-		<label for=\"num\">Si non entrez votre numéro :</label>
+		<label for=\"nutil\">Si non entrez votre numéro :</label>
 		<input type=\"number\" id=\"nUtil\" name=\"nUtil\"></input>
 		<input type=\"submit\" value=\"Valider\"></input>
 		<input type=\"reset\" value=\"Annuler\"></input>
