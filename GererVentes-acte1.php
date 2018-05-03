617d300905bda0163df18ab221a8a40a4ea6f029
requiem958 <requiemandsahariel@gmail.com>
2018-05-03 13:19:10 +0200

    Reorganisation

--- GererVentes-acte1.php
+++ GererVentes-acte1.php
@@ -1,4 +1,35 @@
 <?php session_start();
+
+	
+	//On récupére les variables sur le numéro d'user
+
+	if(isset($_POST['nvutil'])){
+		$requete = 'select max(nUtil)+1 from LesDossiers';
+		$curseur = oci_parse($lien,$requete);
+		$ok = @oci_execute ($curseur) ;
+
+		// on teste $ok pour voir si oci_execute s'est bien passé
+		if (!$ok) {
+			// oci_execute a échoué, on affiche l'erreur
+			$error_message = oci_error($curseur);
+			echo "<p class=\"erreur\">{$error_message['message']}</p>";
+
+		}
+		else {
+			if (!oci_fetch($curseur)){
+				$_SESSION['nUtil'] = 0;
+			}
+			else{
+				$_SESSION['nUtil'] = oci_result($curseur,1);
+			}
+		}
+	}
+	else if (isset($_POST['nutil'])){
+		$_SESSION['nUtil'] = $_POST['nUtil'];
+	}
+
+	//On récupère les numéros de dossier
+
 	$requete = 'select max(nDossier)+1 from LesDossiers_base';
 	$curseur = oci_parse($lien,$requete);
 	$ok = @oci_execute ($curseur) ;
@@ -64,3 +95,4 @@
 	else {
 		echo "<p> Vous n'avez aucun numéro de dossier.</p>":
 	}
+?>
--- GererVentes.php
+++ GererVentes.php
@@ -2,33 +2,7 @@
 
 	$titre = 'Ventes de billets';
 	include('entete.php');
-
-	if(isset($_POST['nvutil'])){
-		$requete = 'select max(nUtil)+1 from LesDossiers';
-		$curseur = oci_parse($lien,$requete);
-		$ok = @oci_execute ($curseur) ;
-
-		// on teste $ok pour voir si oci_execute s'est bien passé
-		if (!$ok) {
-			// oci_execute a échoué, on affiche l'erreur
-			$error_message = oci_error($curseur);
-			echo "<p class=\"erreur\">{$error_message['message']}</p>";
-
-		}
-		else {
-			if (!oci_fetch($curseur)){
-				$_SESSION['nUtil'] = 0;
-			}
-			else{
-				$_SESSION['nUtil'] = oci_result($curseur,1);
-			}
-		}
-	}
-	else if (isset($_POST['nutil'])){
-		$_SESSION['nUtil'] = $_POST['nUtil'];
-	}
-	else{
-		echo ("
+	echo ("
 	<form method=\"post\" action=\"GererVentes-acte1.php\">
 		<label for=\"nvutil\">Êtes vous un nouvel utilisateur ?</label>
 		<input type=\"checkbox\" id=\"yes\" name=\"nvutil\"></input>
@@ -37,6 +11,5 @@
 		<input type=\"submit\" value=\"Valider\"></input>
 		<input type=\"reset\" value=\"Annuler\"></input>
 	</form>");
-	}
 	include('pied.php');
 ?>
