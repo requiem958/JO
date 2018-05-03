a43e0ac553e850d9625be883146cdcbae73e2dd1
requiem958 <requiemandsahariel@gmail.com>
2018-05-03 15:06:59 +0200

    Page finale

--- GererVentes-action.php
+++ GererVentes-action.php
@@ -17,10 +17,13 @@
 			// oci_execute a échoué, on affiche l'erreur
 			$error_message = oci_error($curseur);
 			echo "<p class=\"erreur\">{$error_message['message']}</p>";
+			oci_rollback($curseur)
+			oci_free_statement($curseur);
+			include('pied.php');
+			return;
 
 		}
 		else {
-			oci_free_statement($curseur);
 			$curseur = oci_parse($lien,'select max(nBillet)+1 from Lesbillets');
 			$ok = @oci_execute ($curseur) ;
 
@@ -30,7 +33,11 @@
 				// oci_execute a échoué, on affiche l'erreur
 				$error_message = oci_error($curseur);
 				echo "<p class=\"erreur\">{$error_message['message']}</p>";
+				oci_rollback($curseur);
 
+				oci_free_statement($curseur);
+				include('pied.php');
+				return;
 			}
 			else {
 				if (!oci_fetch($curseur))
@@ -38,15 +45,43 @@
 				else
 					$nBillet = oci_result($curseur);
 
-				oci_free_statement($curseur);
+				//Iteration sur toutes les epreuves demandées
+				foreach($_POST['epreuve'] as $nEpreuve => list($name, $nbBillet)){
+					if ($name == "on"){
+						//Ajout de autant de billets que demandés
+						for ($i=0; $i < $nbBillet; $i++){
+							$curseur = oci_parse($lien,'INSERT INTO LesBillets values(:nBillet,:nDossier,:nEpreuve)');
+							oci_bind_by_name($curseur, ':nBillet', $nBillet+$i)
+							oci_bind_by_name($curseur, ':nDossier', $_SESSION['nDossier']);
+							oci_bind_by_name($curseur, ':nEpreuve', $nEpreuve);
+							$ok = @oci_execute ($curseur);
 
-				var_dump($_POST['epreuve']);
+							if (!$ok) {
+								echo "<p>Erreur insertion billet</p>";
+								// oci_execute a échoué, on affiche l'erreur
+								$error_message = oci_error($curseur);
+								echo "<p class=\"erreur\">{$error_message['message']}</p>";
+								oci_rollback($curseur);
+
+								oci_free_statement($curseur);
+								include('pied.php');
+								return;
+
+							}
+						}
+						$nBillet = $nBillet + $nbBillet;
+					}
+				}
 			}
 		}
 	}
-	else
+	else{
 		echo "<p>Vous n'avez rien à faire ici.</p>";
-
+		include('pied.php');
+		return;
+	}
+	oci_commit($curseur);
 	oci_free_statement($curseur);
+
 	include('pied.php');
 ?>
