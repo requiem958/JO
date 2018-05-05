998f2a16b83a66d1b866d617339b2500c2d3303e
Zarukin <37689537+Zarukin@users.noreply.github.com>
2018-05-05 23:54:09 +0200

    ficher fonctionnel commenté prêt pour le rendue

--- GererEpreuves.php
+++ GererEpreuves.php
@@ -2,8 +2,8 @@
     $titre = 'ajouter une epreuve';
 	include('entete.php');
 	$requete = ("
-		SELECT discipline from LesDisciplines 
-	");
+		SELECT discipline from LesDisciplines   
+	");//on selectione toute les discplines pour pouvoir faire le formulaire 
 	// analyse de la requete et association au curseur
 	$curseur = oci_parse ($lien, $requete) ;
 	// execution de la requete
@@ -18,24 +18,24 @@
 		// oci_execute a réussi, on fetch sur le premier résultat
 		$res = oci_fetch ($curseur);
 		if (!$res) {
-			// il n'y a aucun résultat
+			// il n'y a aucun résultat du coup on demande directement à l'utilisateur d'entrez une nouvelle discpline
 			echo(" 
           <form action=\"creationEpreuves.php\" method=\"POST\">
           <p>Entrez le nom de l'épreuve :<input type=\"text\" name=\"NomE\" /></p>
           <label for=\"inp_categorie\">Veuillez choisir une categorie :</label>
           <select name=\"categorie\">
-			 <option value=\"Mixte\">Mixte</option>
-			 <option value=\"Feminin\">Feminin</option>
-			 <option value=\"Masculin\">Masculin</option>
+			 <option value=\"mixte\">Mixte</option>
+			 <option value=\"feminin\">Feminin</option>
+			 <option value=\"masculin\">Masculin</option>
 			</select>
 	<p><label for=\"inp_forme\">Veuillez choisir une forme :</label>
           <select name=\"forme\">
-			 <option value=\"Individuel\">Individuel</option>
-			 <option value=\"Equipe\">Equipe</option>
-			 <option value=\"couple\">Couple</option>
+			 <option value=\"Individuelle\">Individuel</option>
+			 <option value=\"par equipe\">Equipe</option>
+			 <option value=\"par couple\">Couple</option>
 			</select></p>
 		<p>            Entrez une discipline puisqu'aucune existe :<input type=\"text\" name=\"inp_discipline\" /></p>
-		<p>            Entrez la date :<input type=\"date\" name=\"Date\" /></p>
+		<p>            Entrez la date :<input type=\"date\" name=\"Datep\" /></p>
 		<p>            Entrez le prix :<input type=\"number\" name=\"Prix\" min=\"0\" /></p>
 		<p>            Entrez le nombre par équipe :<input type=\"number\" name=\"nbs\" min=\"1\" /></p>
 			<br /><br />
@@ -44,22 +44,22 @@
 		</form>
 	");
 		}
-		//cas ou il y'a des résultat
+		//cas ou il y'a des résultat on demande à l'utilisateur de tout saisir 
 		else {
 	echo("
           <form action=\"creationEpreuves.php\" method=\"POST\">
           <p>Entrez le nom de l'épreuve :<input type=\"text\" name=\"NomE\" /></p>
           <label for=\"inp_categorie\">Veuillez choisir une categorie :</label>
           <select name=\"categorie\">
-			 <option value=\"Mixte\">Mixte</option>
-			 <option value=\"Feminin\">Feminin</option>
-			 <option value=\"Masculin\">Masculin</option>
+			 <option value=\"mixte\">Mixte</option>
+			 <option value=\"feminin\">Feminin</option>
+			 <option value=\"masculin\">Masculin</option>
 			</select>
 	<p><label for=\"inp_forme\">Veuillez choisir une forme :</label>
           <select name=\"forme\">
-			 <option value=\"Individuel\">Individuel</option>
-			 <option value=\"Equipe\">Equipe</option>
-			 <option value=\"couple\">Couple</option>
+			 <option value=\"individuelle\">Individuel</option>
+			 <option value=\"par equipe\">Equipe</option>
+			 <option value=\"par couple\">Couple</option>
 			</select></p>
 			
 			<p><label for=\"inp_discipline\"> choisir une discipline :</label>
@@ -73,7 +73,7 @@
 		
 		echo ("			</select></p> 
 		   <p>   Entrez la nouvelle discipline :<input type=\"text\" name=\"Disc\" /></p>
-		<p>            Entrez la date (DD-MM-YYYY) :<input type=\"text\" name=\"Date\" /></p>
+		<p>            Entrez la date (DD-MM-YYYY) :<input type=\"text\" name=\"Datep\" /></p>
 		<p>            Entrez le prix :<input type=\"number\" name=\"Prix\" min=\"0\" /></p>
 		<p>            Entrez le nombre par équipe :<input type=\"number\" name=\"nbs\" min=\"1\" /></p>
 			<br /><br />
--- creationEpreuves.php
+++ creationEpreuves.php
@@ -1,89 +1,86 @@
-
 <?php
 	$nome=$_POST['NomE'];
-	$date=$_POST['Date'];
+	$datep=$_POST['Datep'];
 	$categorie=$_POST['categorie'];
 	$prix=$_POST['Prix'];
 	$forme=$_POST['forme'];
 	$disc=$_POST['Disc'];
 	$discipline=$_POST['inp_discipline'];
 	$nbs=$_POST['nbs'];
-	if ($discipline =="" ||$nome=="" || $date=="" || $prix=="" || $nbs =="" ){
+	if ( $discipline=="0" and $disc==""  || $nome=="" || $datep=="" || $prix=="" || $nbs =="" ){ // cas ou il nous manque un arguments
 		$titre = "Erreur un champ est vide";
-	include('entete.php');
-
-	include('pied.php');
-	}
-		if ($discipline =="0"){
-	$discipline=$disc;
-	$requete1 = "INSERT INTO LesDisciplines values ':d'";
-	// analyse de la requete et association au curseur
-	$curseur = oci_parse ($lien, $requete1) ;
-	// affectation de la variable
-	oci_bind_by_name ($curseur,':d', $discipline);
-	// execution de la requete
-$ok = @oci_execute ($curseur) ;
-if (!$ok) {
-		// oci_execute a échoué, on affiche l'erreur
-		$error_message = oci_error($curseur);
-		echo "<p class=\"erreur\">{$error_message['message']}</p>";
-	}
-}
+		include('entete.php');
+		include('pied.php');
+	} 
 	else {
-	$titre = "L'épreuve  $nome ,$categorie ,$forme($nbs) ,$discipline ,$date , $prix à été crée ";
-	include('entete.php');
- $requete = ("
-		SELECT max(Nepreuve) from Lesepreuves
-	");
+		if ($discipline =="0"){  // cas ou on crée une nouvelle disciplines
+			$count=1;  //conteur permetant de savoir si nous sommes passez par cette boucle
+			$discipline=$disc;//on assigne à discipline la valeur saisie par l'utilisateur
+			$titre = "L'épreuve  $nome ,$categorie ,$forme($nbs) ,$discipline ,$datep , $prix à été crée ";
+			include('entete.php');
+			$requete1 = "INSERT INTO LesDisciplines values (:d )"; //requetes pour insérer une nouvelle discipline
+			// analyse de la requete et association au curseur
+			$curseur = oci_parse ($lien, $requete1) ;
+			// affectation de la variable
+			oci_bind_by_name ($curseur,':d', $discipline);
+			// execution de la requete
+			$ok = @oci_execute ($curseur) ;
+			if (!$ok) {
+				echo LeMessage ("majRejetee") ;
+				// terminaison de la transaction : annulation
+				oci_rollback ($lien) ;
+			}	else {
+				echo (" tout vas bien ") ;
+				// terminaison de la transaction : validation
+				oci_commit ($lien) ;
+			}
+		}
+		if ($count!=1){  // puisque nous somme pas passez plus haut on execute ici le titre
+		$titre = "L'épreuve  $nome ,$categorie ,$forme($nbs) ,$discipline ,$datep , $prix à été crée ";
+		include('entete.php');
+		}
+		 $requete = ("
+		SELECT max(Nepreuve)+1 from Lesepreuves
+		"); // on récupére le nepreuve au quels on est et on ajoute 1 pour celui qu'on va crée 
 		// analyse de la requete et association au curseur
-	$curseur = oci_parse ($lien, $requete) ;
-	// execution de la requete
-$ok = @oci_execute ($curseur) ;
-	// on teste $ok pour voir si oci_execute s'est bien passé
-	if (!$ok) {
-		// oci_execute a échoué, on affiche l'erreur
-		$error_message = oci_error($curseur);
-		echo "<p class=\"erreur\">{$error_message['message']}</p>";
-	}
-	$nepreuve = $requete+1;
-	
-	$requete2 = "INSERT INTO LesEpreuves values (:nepreuve, ':nome', ':forme', ':discipline', ':categorie', :nbs, to_date(':date', 'DD-MM-YYYY'), )";
-	// analyse de la requete et association au curseur
-	$curseur = oci_parse ($lien, $requete2) ;
-	// affectation de la variable
-	oci_bind_by_name ($curseur,':nepreuve', $nepreuve);
-	oci_bind_by_name ($curseur,':nome', $nome);
-	oci_bind_by_name ($curseur,':forme', $forme);
-	oci_bind_by_name ($curseur,':discipline', $discipline);
-	oci_bind_by_name ($curseur,':categorie', $categorie);
-	oci_bind_by_name ($curseur,':nbs', $nbs);
-	oci_bind_by_name ($curseur,':date', $date);
-	// execution de la requete
-$ok = @oci_execute ($curseur) ;
-if (!$ok) {
-        echo $requete1;
-		echo LeMessage ("majRejetee");
-		// terminaison de la transaction : annulation
-		oci_rollback ($lien) ;
-	}	else {
-		// analyse de la requete 2 et association au curseur
-		$curseur = oci_parse ($lien, $requete2) ;
+		$curseur = oci_parse ($lien, $requete) ;
 		// execution de la requete
-		$ok = oci_execute ($curseur, OCI_NO_AUTO_COMMIT) ;
+		$ok = @oci_execute ($curseur) ;
 		// on teste $ok pour voir si oci_execute s'est bien passé
 		if (!$ok) {
-			echo LeMessage ("majRejetee") ;
-			// terminaison de la transaction : annulation
-			oci_rollback ($lien) ;
-		}		else {
-			echo LeMessage ("maj OK") ;
-			// terminaison de la transaction : validation
-			oci_commit ($lien) ;
+			// oci_execute a échoué, on affiche l'erreur
+			$error_message = oci_error($curseur);
+			echo "<p class=\"erreur\">{$error_message['message']}</p>";
 		}
-	}
+		oci_fetch ($curseur);
+		$nepreuve= oci_result($curseur,1);
+		$requete2 = "INSERT INTO LesEpreuves values (:nepreuve, :nome, :forme, :discipline, :categorie, :nbs, to_date(:datep, 'DD-MM-YYYY'),:prix)";
+		// analyse de la requete et association au curseur
+		$curseur = oci_parse ($lien, $requete2) ;
+		// affectation des variables
+		oci_bind_by_name ($curseur,':nepreuve', $nepreuve);
+		oci_bind_by_name ($curseur,':nome', $nome);
+		oci_bind_by_name ($curseur,':forme', $forme);
+		oci_bind_by_name ($curseur,':discipline', $discipline);
+		oci_bind_by_name ($curseur,':categorie', $categorie);
+		oci_bind_by_name ($curseur,':nbs', $nbs);
+		oci_bind_by_name ($curseur,':datep', $datep);
+		oci_bind_by_name ($curseur,':prix', $prix);
+		// execution de la requete
+		$ok = @oci_execute ($curseur,OCI_NO_AUTO_COMMIT) ;
+			if (!$ok) {
+				echo LeMessage ("majRejetee") ;
+				// terminaison de la transaction : annulation
+				oci_rollback ($lien) ;
+			}	else {
+				echo (" confirmation de la creation ") ;
+				// terminaison de la transaction : validation
+				oci_commit ($lien) ;
+			}
+
 	// on libère le curseur
 	oci_free_statement($curseur);
 	include('pied.php');
-}
+	}
 
 ?>
